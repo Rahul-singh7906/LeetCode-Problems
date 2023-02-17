367b6f18e85bcc6ff785ebe75fd9f4d06ef8eb1e
Rahul Singh <91553428+Rahul-singh7906@users.noreply.github.com>
2023-02-17 23:22:14 +0530

    Time: 186 ms (5.04%), Space: 43 MB (74.60%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.java
@@ -0,0 +1,38 @@
+class Solution {
+    List<String> ls = new ArrayList<>();
+    public List<String> letterCasePermutation(String s) {
+        helper(s,0);
+        return ls;
+    }
+    public void helper(String s,int ind){
+        if(ind==s.length()){
+            if(!ls.contains(s)) ls.add(s);
+            return ;
+        }
+        if(ind>s.length()) return;
+        helper(s,ind+1);
+        char ch=s.charAt(ind);
+        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
+            // System.out.println(s);
+            s=changer(s,ind);
+            // System.out.println(s);
+            helper(s,ind+1);
+        }
+    }
+    public String changer(String str,int ind){
+        char[] ch = str.toCharArray();
+        if(ch[ind]>='a' && ch[ind]<='z'){
+            // System.out.println("loop1");
+            ch[ind]=Character.toUpperCase(ch[ind]);
+        }
+        else {
+            ch[ind]=Character.toLowerCase(ch[ind]);
+            // System.out.println("loop2");
+        }
+        String ans="";
+        for(int i=0;i<ch.length;i++){
+            ans+=ch[i];
+        }
+        return ans;
+    }
+}
