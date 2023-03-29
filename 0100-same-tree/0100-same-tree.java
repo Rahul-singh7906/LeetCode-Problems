a1aa32b5954509a7d980f59179fcc57f6a4794e2
Rahul Singh <91553428+Rahul-singh7906@users.noreply.github.com>
2023-03-29 23:50:21 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (26.26%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,6 +18,8 @@ class Solution {
         if(p==null && q==null) return true;
         if(p==null || q==null) return false;
         if(p.val!=q.val) return false;
-        return isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+        boolean lf=isSameTree(p.left,q.left);
+        boolean rt=isSameTree(p.right,q.right);
+        return lf&&rt;
     }
 }
