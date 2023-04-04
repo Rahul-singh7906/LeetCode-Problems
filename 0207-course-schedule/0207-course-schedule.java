a7b34aeea6d7eebc9740637f66626e2701169c28
Rahul Singh <91553428+Rahul-singh7906@users.noreply.github.com>
2023-04-04 18:19:30 +0530

    Time: 7 ms (37.26%), Space: 43.4 MB (21.47%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,35 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if(prerequisites.length==0) return true;
+    public boolean canFinish(int num, int[][] pr) {
+        if(pr.length==0) return true;
         List<List<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
+        for(int i=0;i<num;i++){
             adj.add(new ArrayList<Integer>());
         }
-        for(int i=0;i<prerequisites.length;i++){
-            int u=prerequisites[i][0];
-            int v=prerequisites[i][1];
+        for(int i=0;i<pr.length;i++){
+            int u=pr[i][0];
+            int v=pr[i][1];
             adj.get(v).add(u);
         }
-        int[] in= new int[numCourses];
-            for(int i=0;i<numCourses;i++){
-                for(int it:adj.get(i)){
-                    in[it]++;
-                }
-            }
-        Queue<Integer> q = new LinkedList<>();
-        
-        for(int i=0;i<numCourses;i++){
-            if(in[i]==0) {
+        Queue<Integer> q=new LinkedList<>();
+        int[] indegree=new int[num];
+        for(int i=0;i<adj.size();i++){
+            for(int it:adj.get(i)) indegree[it]++;
+        }
+        for(int i=0;i<num;i++){
+            if(indegree[i]==0){
                 q.add(i);
             }
         }
-        int sz=0;
+        List<Integer> ls = new ArrayList<>();
         while(!q.isEmpty()){
-            int v=q.poll();
-            sz++;
-            for(int it:adj.get(v)){
-                in[it]--;
-                if(in[it]==0) q.add(it);
+            int node=q.poll();
+            ls.add(node);
+            for(int it:adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0) q.add(it);
             }
         }
-        if(sz==numCourses) return true;
+        if(ls.size()==num) return true;
         return false;
     }
 }
