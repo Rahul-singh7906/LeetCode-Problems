a0aab90f17f97113be6212e7557c8af416538a26
Rahul Singh <91553428+Rahul-singh7906@users.noreply.github.com>
2023-06-04 18:03:56 +0530

    Time: 1 ms (98.97%), Space: 44.2 MB (18.04%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,47 +1,22 @@
-class Disjoint{
-    int[] parent;
-    int[] size;
-    Disjoint(int n){
-        parent=new int[n+1];
-        size=new int[n+1];
-        for(int i=0;i<=n;i++){
-            parent[i]=i;
-            size[i]=1;
-        }
-    }
-    public int findUlParent(int node){
-        if(parent[node]==node) return node;
-        return parent[node]=findUlParent(parent[node]);
-    }
-    public void unionBySize(int u,int v){
-        int ulp_u=findUlParent(u);
-        int ulp_v=findUlParent(v);
-        if(ulp_u==ulp_v) return;
-        if(size[ulp_u]<size[ulp_v]){
-            parent[ulp_u]=ulp_v;
-            size[ulp_v]+=size[ulp_u];
-        }
-        else{
-            parent[ulp_v]=ulp_u;
-            size[ulp_u]+=size[ulp_v];
-        }
-    }
-}
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
+    public int findCircleNum(int[][] isC) {
+        int n=isC.length;
+        int[] vis=new int[n];
         int ct=0;
-        int n=isConnected.length;
-        Disjoint ds=new Disjoint(n);
-        for(int i=0;i<isConnected.length;i++){
-            for(int j=0;j<isConnected.length;j++){
-                if(isConnected[i][j]==1){
-                    ds.unionBySize(i+1,j+1);
-                }
+        for(int i=0;i<n;i++){
+            if(vis[i]==0){
+                ct++;
+                dfs(isC,vis,i+1);
             }
         }
-        for(int i=1;i<=n;i++){
-            if(ds.parent[i]==i) ct++;
-        }
         return ct;
+    }
+    public void dfs(int[][] grid,int[] vis,int node){
+        vis[node-1]=1;
+        for(int j=0;j<grid.length;j++){
+            if(grid[node-1][j]==1){
+                if(vis[j]==0) dfs(grid,vis,j+1);
+            }
+        }
     }
 }
